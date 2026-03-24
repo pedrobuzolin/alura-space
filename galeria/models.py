6f94aaefaf53063ad0dbf21c82762bd6e2ffe083
pedrobuzolin <buzolinpedro@gmail.com>
2026-03-23 21:38:49 -0300

    Adicionando banco de dados

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Fotografia(models.Model):
+
+    CATEGORIAS = [
+        ("NEBULOSA", "Nebulosa"),
+        ("GALÁXIA", "Galáxia"),
+        ("ESTRELA", "Estrela"),
+        ("PLANETA", "Planeta")
+    ]
+
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=CATEGORIAS, default="")
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return f"Fotografia [nome: {self.nome}]"
